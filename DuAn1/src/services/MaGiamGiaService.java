939c14b0477a1271810b867a60e4036baee5ea8a
Hoi Dan IT with Eric <duynvph30146@fpt.edu.vn>
2023-07-16 17:12:39 +0700

    duy update service

---
+++ DuAn1/src/services/MaGiamGiaService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import interfaceservices.IMaGiamGiaService;
+
+/**
+ *
+ * @author Admin
+ */
+public class MaGiamGiaService implements IMaGiamGiaService{
+    
+}
---
+++ DuAn1/src/services/SanPhamService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import interfaceservices.ISanPhamService;
+
+/**
+ *
+ * @author Admin
+ */
+public class SanPhamService implements ISanPhamService{
+    
+}
