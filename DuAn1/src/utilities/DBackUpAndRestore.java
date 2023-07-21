2d720d75ad18fd3e8c74353afedd67e1e3d4a08b
Doanhtklv <Doanhtklv@gmail.com>
2023-07-21 20:07:10 +0700

    Tạo file commit

--- DuAn1/src/utilities/DBackUpAndRestore.java
+++ DuAn1/src/utilities/DBackUpAndRestore.java
@@ -10,80 +10,90 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class DBackUpAndRestore {
-	private static void createFolder() {
-		File dirBackup = new File("C:\\ToTo\\Backup");
-		if (!dirBackup.exists()) {
-			dirBackup.mkdirs();
-		}
-	}
 
-	public static void createBackup() {
-		createFolder();
-		File src = new File("C:\\ToTo\\Backup");
-		if (!src.isDirectory() || !src.exists())
-			return ;
-		try {
-			Connection conn = JdbcHelper.getConnection();
-			Statement st = conn.createStatement();
-			String querry = ""
-					+ "Begin try " 
-					+ "DECLARE @FileName varchar(100) "
-					+ "Set @FileName=CONCAT('C:\\ToTo\\Backup\\',CONCAT_WS('_',YEAR(GETDATE()),Month(GETDATE()),DAY(GETDATE()),"
-					+ "'__',DATEPART(HOUR,GETDATE()),DATEPART(MINUTE,GETDATE()),DATEPART(SECOND,GETDATE())),'.bak') "
-					+ "BACKUP DATABASE ToTo "
-					+ "TO DISK = @FileName " 
-					+ "SELECT @FileName " 
-					+ "END TRY " 
-					+ "BEGIN CATCH "
-					+ "SELECT ''" 
-					+ "END CATCH";
-			ResultSet rs = st.executeQuery(querry);
-			rs.next();
-			String mss=rs.getString(1);
-			JOptionPane.showMessageDialog(null,mss.equals("")?"Backup thất bại":"<html>Backup thành công<hr>File được lưu tại:<br>"+mss+"</html>");
-			conn.close();
+    private static void createFolder() {
+        File dirBackup = new File("C:\\ToTo\\Backup");
+        if (!dirBackup.exists()) {
+            dirBackup.mkdirs();
+        }
+    }
 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+    public static void createBackup() {
+        createFolder();
+        File src = new File("C:\\ToTo\\Backup");
+        if (!src.isDirectory() || !src.exists()) {
+            return;
+        }
+        try {
+            Connection conn = JdbcHelper.getConnection();
+            Statement st = conn.createStatement();
+            String querry = ""
+                    + "Begin try "
+                    + "DECLARE @FileName varchar(100) "
+                    + "Set @FileName=CONCAT('C:\\ToTo\\Backup\\',CONCAT_WS('_',YEAR(GETDATE()),Month(GETDATE()),DAY(GETDATE()),"
+                    + "'__',DATEPART(HOUR,GETDATE()),DATEPART(MINUTE,GETDATE()),DATEPART(SECOND,GETDATE())),'.bak') "
+                    + "BACKUP DATABASE ToTo "
+                    + "TO DISK = @FileName "
+                    + "SELECT @FileName "
+                    + "END TRY "
+                    + "BEGIN CATCH "
+                    + "SELECT ''"
+                    + "END CATCH";
+            ResultSet rs = st.executeQuery(querry);
+            rs.next();
+            String mss = rs.getString(1);
+            JOptionPane.showMessageDialog(null, mss.equals("") ? "Backup thất bại" : "<html>Backup thành công<hr>File được lưu tại:<br>" + mss + "</html>");
+            conn.close();
 
-	}
-	public static List<String> getListFileBackUp(){
-		List<String> lstFileName=new ArrayList<>();
-		File dir=new File("C:\\ToTo\\Backup");
-		for(File file:dir.listFiles()) {
-			lstFileName.add(file.getName());
-		}
-		return lstFileName;
-	}
-	public static void restoreDatabase(String fileName) {
-		try {
-			
-			Connection conn=JdbcHelper.getConnection("master");
-			Statement st=conn.createStatement();
-			var querry="BEGIN TRY "
-					+ "Restore database ToTo "
-					+ "From disk= 'C:\\ToTo\\Backup\\"+fileName+"' "
-					+ "with replace "
-					+ "SELECT 1 "
-					+ "END TRY "
-					+ "BEGIN CATCH "
-					+ "SELECT 0 "
-					+ "END CATCH";
-			ResultSet rs=st.executeQuery(querry);
-			if(rs.next()) {
-				JOptionPane.showMessageDialog(null, rs.getInt(1)==1?"Backup thành công ! Vui lòng khởi động lại trương trình":"Backup thất bại");
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	public static void main(String[] args) {
-		createBackup();
-//		restoreDatabase("2023_7_18____15_53_42.bak");
-	}
+    }
+
+    public static List<String> getListFileBackUp() {
+        List<String> lstFileName = new ArrayList<>();
+        File dir = new File("C:\\ToTo\\Backup");
+        for (File file : dir.listFiles()) {
+            lstFileName.add(file.getName());
+        }
+        return lstFileName;
+    }
+
+    public static void restoreDatabase(String fileName) {
+        try {
+
+            Connection conn = JdbcHelper.getConnection("master");
+            Statement st = conn.createStatement();
+            var querry = ""
+                    + "DECLARE @kill varchar(8000) = ''; "
+                    + "SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';' "
+                    + "FROM sys.dm_exec_sessions "
+                    + "WHERE database_id  = db_id('ToTo') "
+                    + "print @kill "
+                    + "exec(@kill) "
+                    + "BEGIN TRY "
+                    + "Restore database ToTo "
+                    + "From disk= 'C:\\ToTo\\Backup\\" + fileName + "' "
+                    + "with replace "
+                    + "SELECT 1 "
+                    + "END TRY "
+                    + "BEGIN CATCH "
+                    + "SELECT 0 "
+                    + "END CATCH";
+            ResultSet rs = st.executeQuery(querry);
+            
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, rs.getInt(1) == 1 ? "Backup thành công ! Vui lòng khởi động lại trương trình" : "Backup thất bại");
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public static void main(String[] args) {
+		restoreDatabase("2023_7_18____15_53_42.bak");
+    }
 
 }
